82be79c70a2fcc919cf40f533bfa7faf89f01e3a
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-11-28 15:12:55 -0800

    copy cut and paste 2

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -6,11 +6,3 @@ middle_word = "olberto"
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
-
-word = "School"
-word_first_3 = "Sch"
-word_last_2 = "ol"
-middle_word = "choo"
-print("First 3 letters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))

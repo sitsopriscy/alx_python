eee814483fea76e1c3133b0f1614c73fb0906643
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-11-28 15:18:58 -0800

    ccp2

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = "Sch"
-word_last_2 = "ol"
-middle_word = "choo"
+word_first_3 = "Hol", "Sch"
+word_last_2 = "on", "ol"
+middle_word = "olberto", "choo"
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))

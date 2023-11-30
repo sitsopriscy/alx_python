69a731a55c4e3205d5504cb979cd7d6cf34d16d2
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-11-30 11:38:13 -0800

    00...99

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,3 @@
 for number in range(100):
     if number < 99:
-       print("{:02}, ".format(number), end='', flush=True)
-    else:
-        print("{:02}".format(number))
-        
+        print("{:2}, ".format(number), end="", flush=True)

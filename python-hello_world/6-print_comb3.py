05874d5b224094a0e74f3e66cce6f600efa64aa9
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-11-30 12:15:01 -0800

    Inventing is a combination 1

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -2,6 +2,10 @@ for tens_digit in range(10):
     for units_digit in range(tens_digit + 1, 10):
         if units_digit ==9 and tens_digit == 8:
              print("{:d}{:d}".format(tens_digit, units_digit), end="", flush=True)
+
         else:
             print("{:d}{:d},".format(tens_digit, units_digit), end="", flush=True)
+
+        # giving space
+            print()
             

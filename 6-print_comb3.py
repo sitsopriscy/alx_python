396a86ed60e202c84b7c37df1688c9de4404816a
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-11-30 12:06:51 -0800

    Inventing is a combination

--- 6-print_comb3.py
+++ 6-print_comb3.py
@@ -4,3 +4,4 @@ for tens_digit in range(10):
              print("{:d}{:d}".format(tens_digit, units_digit), end="", flush=True)
         else:
             print("{:d}{:d},".format(tens_digit, units_digit), end="", flush=True)
+            

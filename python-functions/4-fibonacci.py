f32a60d66239744159945caca1712063d9390132
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-11-30 13:17:06 -0800

    Fibonacci Sequence Function

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,8 @@
+def fibonacci_sequence(n):
+    fib_sequence = [0, 1]
+
+    while len(fib_sequence) < n:
+        next_term = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_term)
+
+    return fib_sequence[:n]

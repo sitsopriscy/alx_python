f4f8dd3022ec25d7e890c97a57349a1942f6cfe9
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-01 22:48:30 -0800

    add44

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -7,5 +7,5 @@ def add_0(a, b):
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0))
+    print("{} + {} = {}".format(a, b, add_0(a, b)))
     # print(f"{a} + {b} = {(add(a,b))}")

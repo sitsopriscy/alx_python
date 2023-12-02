7f6e6e9008d0271bba4efcc10179a0ec35419cf4
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-01 22:45:47 -0800

    add234

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
 
     a = 1
     b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
     # print(f"{a} + {b} = {(add(a,b))}")

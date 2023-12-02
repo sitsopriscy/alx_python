f168be1426ca8cc9f2d3368042c57f67ce9fea76
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-01 22:50:40 -0800

    add223

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 # import random
 # number = random.randint(-10, 10)
 
-def add_0(a, b):
+def add(a, b):
     return a + b
 
+
 if __name__ == "__main__":
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add_0(a, b)))
+    a, b = 1, 2
+    print(f"{a} + {b} = {add(a, b)}")
     # print(f"{a} + {b} = {(add(a,b))}")

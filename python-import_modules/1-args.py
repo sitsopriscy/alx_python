cb6522cd063321566b56732ba3848751d4b8b4be
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-03 20:47:33 -0800

    33args

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,20 +1,23 @@
 import sys
 
 def print_arguments():
-    num_arguments = len(sys.argv) - 1 
+    # Get the number of arguments
+    num_arguments = len(sys.argv) - 1  # Exclude the script name itself
 
+    # Print the number of arguments
     if num_arguments == 0:
         print("0 argument.")
+        print(".")
     elif num_arguments == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(num_arguments))
 
+    # Print each argument and its position
     for i, arg in enumerate(sys.argv[1:], start=1):
         print("{}: {}".format(i, arg))
-
+     
+        print("[stderr]: [Anything]")
+        
 if __name__ == "__main__":
     print_arguments()
-
-    print("[stderr]: [Anything]")
-    

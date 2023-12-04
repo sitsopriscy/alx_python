3d8e2906b22e6ece1bb24fec0292c0331a0b2d5a
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-03 18:33:09 -0800

    ad1112

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
 
-if __name__ == "__main__":
-    a, b = 1, 2
-    result_string = "{} + {} = {}".format(a, b, add(a, b))
-    print(result_string)
+def add(a, b):
+    return a+b 
+
+a = 1
+b = 2
+
+# Import the add function from add_0.py
+# from add_0 import add
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+
+# def add(a, b):
+#     return a + b
+
+# if __name__ == "__main__":
+#     a, b = 1, 2
+#     result_string = "{} + {} = {}".format(a, b, add(a, b))
+#     print(result_string)
 
     # print(f"{a} + {b} = {(add(a,b))}")

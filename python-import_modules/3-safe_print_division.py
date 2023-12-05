87d3bc198afba0643933d931203b19419b32f7d5
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-05 13:57:41 -0800

    Integers division with debug13

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -9,5 +9,5 @@ def safe_print_division(a, b):
     return result
 
 # Example usage:
-result = safe_print_division(10, 0)
-print("{:d} / {:d} = {}".format(10, 0, result))
+result = safe_print_division(10, 2)
+print("{:d} / {:d} = {}".format(10, 2, result))

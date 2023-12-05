a086cfd4a22055005d19ab929f333f9942a532ac
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-05 03:35:56 -0800

    986everything

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -2,3 +2,9 @@ from variable_load_7_2 import a
 
 if __name__ == "__main__":
         print(f"a = {98}")
+
+from variable_load_7_0 import a as a_0
+print(a_0)
+
+from variable_load_7_1 import a as a_1
+print(a_1)

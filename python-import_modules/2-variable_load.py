265c31e9af82ea33a6823fd90381296b778c8f57
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-05 04:05:49 -0800

    tryinnng

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,10 +1,8 @@
-from variable_load_7_2 import a
+from variable_load_2 import a
 
 if __name__ == "__main__":
-        print(f"a = {98}")
+        print(f"a = {a}")
 
-from variable_load_7_0 import a as a_0
-print(a_0)
+# import variable_load_2
 
-from variable_load_7_1 import a as a_1
-print(a_1)
+# print(variable_load_2.a)
---
+++ python-import_modules/variable_load_2.py
@@ -0,0 +1,6 @@
+print("Enter any number:")
+a = input(vars)
+
+# print("Hello, " + x)
+# input(number)
+# print(a = 98)

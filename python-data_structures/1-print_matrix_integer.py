754cd335d8d3c4972e43351fa39c11a4300590f1
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-06 21:07:09 -0800

    Lists of lists = Matrix

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, num in enumerate(row):
+            if i == len(row) - 1:
+                print("{:d}".format(num), end="")
+            else:
+                print("{:d} ".format(num), end="")
+        print()
+
+if __name__ == "__main__":
+    example_matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    print_matrix_integer(example_matrix)

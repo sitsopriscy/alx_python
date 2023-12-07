c7f9f90bc433f5a5e09c6bac3f9c9c45b7a161f6
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-06 20:50:32 -0800

    raise message

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,9 +1,14 @@
+# def raise_exception_msg(message=""):
+#     raise NameError(message)
+#     raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
+    
+# try:
+#     raise_exception_msg("")
+# except NameError as ne:
+#     print(ne)
+
 def raise_exception_msg(message=""):
     raise NameError(message)
-    raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
-    
-try:
-    raise_exception_msg("")
-except NameError as ne:
-    print(ne)
 
+if __name__ == "__main__":
+    raise_exception_msg("C is fun")

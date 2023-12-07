b15eb0bbb927f122cb609935f1dc5362144e6e6e
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-06 20:57:41 -0800

    make a script dynamic01

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,20 +1,38 @@
-import sys
-from sys import argv
+# import sys
+# from sys import argv
 
-def print_arguments():
-    num_arguments = len(argv) - 1
+# def print_arguments():
+#     num_arguments = len(argv) - 1
 
-    if num_arguments == 0:
-        print("0 argument.")
-        print(":")
-    elif num_arguments == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(num_arguments))
+#     if num_arguments == 0:
+#         print("0 argument.")
+#         print(":")
+#     elif num_arguments == 1:
+#         print("1 argument:")
+#     else:
+#         print("{} arguments:".format(num_arguments))
 
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+#     for i, arg in enumerate(sys.argv[1:], start=1):
+#         print("{}: {}".format(i, arg))
      
         
+# if __name__ == "__main__":
+#     print_arguments()
+
+
+#!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    print_arguments()
+    argv_len = len(sys.argv) - 1  # Subtract 1 to exclude the script name itself
+
+    # Print the number of arguments
+    print("{} argument{}{}".format(
+        argv_len,
+        's' if argv_len != 1 else '',
+        '.' if argv_len == 0 else ':'
+    ))
+
+    # Print each argument with its position
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg))

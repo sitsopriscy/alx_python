4aee4c39a97bae9aa29c3cb335f2b488824ba554
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-06 20:47:03 -0800

    Raise a message02

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -3,7 +3,7 @@ def raise_exception_msg(message=""):
     raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
     
 try:
-    raise_exception_msg("message")
+    raise_exception_msg("")
 except NameError as ne:
     print(ne)
 

6fd02bf6f5291a13ed9d0303bcfcd54d42416169
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-07 14:55:19 -0800

    Best score

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,4 +1,24 @@
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
+#!/usr/bin/python3
+from update_dictionary import update_dictionary
 
+def print_sorted_dictionary(my_dict):
+    """ Print sorted dictionary """
+    keys = sorted(my_dict.keys())
+    for k in keys:
+        print("{}: {}".format(k, my_dict[k]))
+
+if __name__ == "__main__":
+    # Test the update_dictionary function with additional scenarios
+    a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
+    new_dict = update_dictionary(a_dictionary, 'language', "Python")
+    print_sorted_dictionary(new_dict)
+    print("--")
+    print_sorted_dictionary(a_dictionary)
+
+    print("--")
+    print("--")
+
+    new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+    print_sorted_dictionary(new_dict)
+    print("--")
+    print_sorted_dictionary(a_dictionary)

0af47817fcbcf62140fda245eefc087b4fab1c02
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-07 15:12:28 -0800

    Multiply by using map

---
+++ python-more_data_structures/4-multiply_list_map.py
@@ -0,0 +1,19 @@
+# def multiply_list_map(my_list=[], number=0):
+#     return list(map(lambda x: x * number, my_list))
+
+# multiply_list_map = __import__('4-multiply_list_map').multiply_list_map
+
+# my_list = [1, 2, 3, 4, 6]
+# new_list = multiply_list_map(my_list, 4)
+# print(new_list)
+# print(my_list)
+
+#!/usr/bin/python3
+
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
+
+my_list = [1, 2, 3, 4, 6]
+new_list = multiply_list_map(my_list, 4)
+print(new_list)
+print(my_list)

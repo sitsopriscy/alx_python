c370af8100de18654f00d3ed0c104008b918b609
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-06 20:54:41 -0800

    integers division02

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,17 +1,32 @@
-def safe_print_division(a, b):
+# def safe_print_division(a, b):
     
+#     try:
+#         result = a / b
+#     except ZeroDivisionError:
+#         result = None
+#     finally:
+#         print("Inside result: {}".format(result))
+#     return result
+
+# # Example usage:
+# result = safe_print_division(10, 2)
+# # print("{:d} / {:d} = {}".format(10, 2, result))
+# if result is not None:
+#     print("{} / {} = {}".format(10, 2, result))
+# else:
+#     print("None")
+
+
+def safe_print_division(a, b):
+    result = None
     try:
         result = a / b
     except ZeroDivisionError:
-        result = None
+        pass
     finally:
         print("Inside result: {}".format(result))
-    return result
+        return result
 
-# Example usage:
-result = safe_print_division(10, 2)
-# print("{:d} / {:d} = {}".format(10, 2, result))
-if result is not None:
-    print("{} / {} = {}".format(10, 2, result))
-else:
-    print("None")
+if __name__ == "__main__":
+    safe_print_division(10, 2)
+    safe_print_division(10, 0)

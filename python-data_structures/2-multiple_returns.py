5824c562cd651cde285677d201db578f4f8f0bba
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-06 21:11:30 -0800

    More returns!

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if not sentence:
+        return (0, None)
+    
+    return (len(sentence), sentence[0])
+
+if __name__ == "__main__":
+    sentence = "At Holberton school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))

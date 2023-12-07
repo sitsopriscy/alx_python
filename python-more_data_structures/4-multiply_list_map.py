c51bf85a29b6790403b544f46076d4d576bf0bbf
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-07 15:23:34 -0800

    multiply123

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -2,12 +2,17 @@
 
 #!/usr/bin/python3
 
-multiply_list_map = lambda my_list, number: list(map(lambda x: x * number, my_list))
 
-my_list = [1, 2, 3, 4, 6]
-print(multiply_list_map(my_list, 4))
-print(my_list)
+#!/usr/bin/python3
 
+# multiply_list_map = lambda my_list, number: list(map(lambda x: x * number, my_list))
+
+# my_list = [1, 2, 3, 4, 6]
+# print(multiply_list_map(my_list, 4))
+# print(my_list)
+
+def multiply_list_map(my_list=[], number=0):
+     return list(map(lambda x: x * number, my_list))
 
 
 

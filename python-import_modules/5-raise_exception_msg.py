07a88eb7fcb20cf939470cba73b73419d99a2808
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-06 20:39:39 -0800

    Raise a message again

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,10 +1,18 @@
-def raise_exception_msg(message=""):
-    raise NameError(message)
-    raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
+# def raise_exception_msg(message=""):
+#     raise NameError(message)
+# raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
 
+# try:
+#     raise_exception_msg("C is fun")
+# except NameError as ne:
+#     print(ne)
+
+def raise_exception_msg(message=""):
+    raise NameError("C is fun")
+    raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
+    
 try:
     raise_exception_msg("C is fun")
 except NameError as ne:
     print(ne)
-
 

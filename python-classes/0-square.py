9c7eb287ab64a93329338e34c7701d3a569d9832
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-10 06:27:56 -0800

    Square with size 4

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -14,29 +14,4 @@ class Square:
     def area(self):
         return self.__size ** 2
 
-# my_square = Square(5)
-# print(my_square.area())
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-
-# """ To get 'Square' object has no attribute 'size'
-# """
-# try:
-#     print(my_square.size)  
-# except AttributeError as e:
-#     print(e)
-
-# '''
-# To get 'Square' object has no attribute '__size'
-# '''
-# try:
-#     print(my_square.__size)  
-# except AttributeError as e:
-#     print(e)
-
-# square1 = Square(5)
-# print("Size of square1:", square1.get_size())
-# print("Area of square1:", square1.area())
+my_square = Square(3); print(type(my_square)); print(my_square.__dict__)

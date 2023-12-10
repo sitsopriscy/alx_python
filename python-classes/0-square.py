e045bf41e7bf401e4618871f64a0c73a64cb8a9c
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-10 06:33:11 -0800

    Square with size 7

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -13,8 +13,7 @@ class Square:
 
     def area(self):
         return self.__size
-Square = __import__('0-square').Square
-
+    
 my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)

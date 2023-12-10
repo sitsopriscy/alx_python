67ecc0b8ec461c5e283fc3ad17b8b337722b9f00
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-10 06:25:06 -0800

    Square with size 2

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,25 +1,25 @@
-"""_summary_
-
-Returns:
-    _type_: _description_
-"""
 class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new Square instance with the given size.
+    """
     def __init__(self, size):
-        self.__size = size
-
-    def get_size(self):
-        return self.__size
-
-    def set_size(self, size):
         self.__size = size
 
     def area(self):
         return self.__size ** 2
 
+# my_square = Square(5)
+# print(my_square.area())
 
-# my_square = Square(3)
-# print(type(my_square))
-# print(my_square.__dict__)
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
 
 
 # """ To get 'Square' object has no attribute 'size'

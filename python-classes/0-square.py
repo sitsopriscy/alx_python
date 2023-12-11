7af8c6bdc4e401b2e488bf69ef957f71ee521bd7
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-11 14:58:33 -0800

    Square with size 119

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,29 @@
+# class Square:
+#     """
+#     A class that defines a square.
+
+#     Attributes:
+#         __size (int): The size of the square.
+
+#     Methods:
+#         __init__(self, size): Initializes a new Square instance with the given size.
+#     """
+    
+#     def __init__(self, size):
+#         self.__size = size
+
+#     def get_size(self):
+#         return self.__size
+
+# # mysquare = Square(3) print(type(mysquare)) print(mysquare.dict_)
+    
+# mysquare = Square(3)
+# print(type(mysquare))
+# print(mysquare.__dict__)
+
+
+#!/usr/bin/python3
+
 class Square:
     """
     A class that defines a square.
@@ -8,15 +34,18 @@ class Square:
     Methods:
         __init__(self, size): Initializes a new Square instance with the given size.
     """
-    
+
     def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Parameters:
+            size (int): The size of the square.
+        """
         self.__size = size
 
-    def get_size(self):
-        return self.__size
+# Example usage:
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
 
-# mysquare = Square(3) print(type(mysquare)) print(mysquare.dict_)
-    
-mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.__dict__)

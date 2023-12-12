e1e92092f2d6ad8d4c6b70f908dba996bc3e8d2a
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-12 00:01:56 -0800

    Square with size 098

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -24,25 +24,25 @@
 
 #!/usr/bin/python3
 
-class Square:
-    """
-    A class that defines a square.
+# class Square:
+#     """
+#     A class that defines a square.
 
-    Attributes:
-        __size (int): The size of the square.
+#     Attributes:
+#         __size (int): The size of the square.
 
-    Methods:
-        __init__(self, size): Initializes a new Square instance with the given size.
-    """
+#     Methods:
+#         __init__(self, size): Initializes a new Square instance with the given size.
+#     """
 
-    def __init__(self, size):
-        """
-        Initializes a new Square instance.
+#     def __init__(self, size):
+#         """
+#         Initializes a new Square instance.
 
-        Parameters:
-            size (int): The size of the square.
-        """
-        self.__size = size
+#         Parameters:
+#             size (int): The size of the square.
+#         """
+#         self.__size = size
 
 # Example:
 # my_square = Square(3)
@@ -50,3 +50,29 @@ class Square:
 # print(my_square.__dict__)
 
 
+"""
+This module defines a Square class.
+
+The Square class represents a square geometric shape and has a private
+instance attribute for the size.
+
+Example:
+    square = Square(5)
+    print(square.get_area())  # Output: 25
+"""
+
+class Square:
+    """This class defines a Square.
+
+    Attributes:
+        __size (int): Private instance attribute representing the size of the square.
+    """
+
+    def __init__(self, size):
+        """Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+        

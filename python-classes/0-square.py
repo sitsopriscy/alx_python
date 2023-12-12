80a5a2ed0dc66c8a49c7d49b64130622a7047d71
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-11 23:59:36 -0800

    Square with size 009

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -44,8 +44,9 @@ class Square:
         """
         self.__size = size
 
-# Example usage:
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
+# Example:
+# my_square = Square(3)
+# print(type(my_square))
+# print(my_square.__dict__)
+
 

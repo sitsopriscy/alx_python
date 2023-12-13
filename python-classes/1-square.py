a779f5ec4894ad83d5360236bde8458c9848fdd4
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-13 12:41:20 -0800

    Size validation 02

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -24,21 +24,33 @@
 #             size (int): The size of the square.
 #         """
 #         self.__size = size
+"""
+This module defines a Square class.
 
-#!/usr/bin/python3
+The Square class represents a square geometric shape and has a private
+instance attribute for the size.
+
+Example:
+    square = Square(5)
+    print(square.get_area())  # Output: 25
+"""
 
 class Square:
-    """
-    A class that defines a square.
+    """This class defines a Square.
 
     Attributes:
-        __size (int): The size of the square.
-
-    Methods:
-        __init__(self, size=0): Initializes a new Square instance with the given size.
+        __size (int): Private instance attribute representing the size of the square.
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size):
+        """Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+
         """
         Initializes a new Square instance.
 

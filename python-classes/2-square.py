b8f720ff92dfd160110821dd9d8dc87b7340bc52
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-13 13:07:49 -0800

    Area of a square 02

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -16,13 +16,14 @@ class Square:
         __size (int): Private instance attribute representing the size of the square.
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, area=" "):
         """Initializes a new instance of the Square class.
 
         Args:
             size (int): The size of the square.
         """
         self.__size = size
+        self.__area = area
 
         """
         Initializes a new Square instance.

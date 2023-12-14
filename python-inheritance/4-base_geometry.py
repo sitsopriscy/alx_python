640a4dd273b0f99288b8413cfaa4b6300a2a8e15
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-14 07:37:56 -0800

    Improve Geometry

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,12 @@
+"""Public instance method: def area(self):
+
+    Raises:
+        Exception(area()): message(is not implemented)
+"""
+
+class BaseGeometry:
+    def area(self):
+        
+        """Raise an Exception with the message 'area() is not implemented'."""
+        
+        raise Exception('area() is not implemented')

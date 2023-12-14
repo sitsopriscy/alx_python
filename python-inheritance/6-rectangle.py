eb6504aa916898fe12df8806f5a37eff8fc814f6
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-14 14:32:23 -0800

    rectangle 04

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -97,23 +97,23 @@ class Rectangle(BaseGeometry):
     #     return f"Rectangle({self.__width}, {self.__height})"
 
 
-r = Rectangle(3, 5)
+# r = Rectangle(3, 5)
 
-print(r)
-print(dir(r))
+# print(r)
+# print(dir(r))
 
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# try:
+#     print("Rectangle: {} - {}".format(r.width, r.height))
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
 
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# try:
+#     r2 = Rectangle(4, True)
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
     
-rectangle = Rectangle(width=5, height=10)
-attributes = dir(rectangle)
-has_width_attribute = '_Rectangle__width' in attributes
+# rectangle = Rectangle(width=5, height=10)
+# attributes = dir(rectangle)
+# has_width_attribute = '_Rectangle__width' in attributes
 
-print(has_width_attribute)
+# print(has_width_attribute)

b49777edc900c38d5ba2cc68a887f2fd1aa39851
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-14 07:24:08 -0800

    Only sub class of 01

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -6,4 +6,5 @@ def inherits_from(obj, a_class):
     
     """Check if the object is an instance of a class that inherited (directly or indirectly) from the specified class."""
     
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) !=a_class
+

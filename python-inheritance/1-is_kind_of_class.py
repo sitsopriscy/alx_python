da1ae25e92df348532cd711fb9f6597def56f7e3
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-14 07:20:55 -0800

    Only sub class of

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,6 +1,6 @@
 """ This module provides a function that returns True if the object is an instance of,
     or if the object is an instance of a class that inherited 
-    from, the specified class ; otherwise False
+    from, the specified class ; otherwise False.
 """
 
 def is_kind_of_class(obj, a_class):
---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,9 @@
+""" This module provides a function that returns True if the object is an 
+    instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.
+"""
+
+def inherits_from(obj, a_class):
+    
+    """Check if the object is an instance of a class that inherited (directly or indirectly) from the specified class."""
+    
+    return issubclass(type(obj), a_class)

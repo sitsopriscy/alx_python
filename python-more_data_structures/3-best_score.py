ae7cceea50fa89501cf6f66637dd4aaf7e8f9364
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-16 02:04:32 -0800

    best score 01

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == 0:
+    if a_dictionary:
+        return max(a_dictionary) 
+    else:
         return None
-
 #!/usr/bin/python3
 
 # def best_score(a_dictionary):

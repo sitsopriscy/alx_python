be77555b3c62a9a70a83ef37a9b978758c4c4a47
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-16 01:39:03 -0800

    square matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    result_matrix = [[x**2 for x in row] for row in matrix]
-    return result_matrix
+    new_matrix = [[x**2 for x in row] for row in matrix]
+    return new_matrix
 
-if __name__ == "__main__":
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
+#!/usr/bin/python3
 
-    result = square_matrix_simple(matrix)
-    print(result)
+# def square_matrix_simple(matrix=[]):
+#     result_matrix = [[x**2 for x in row] for row in matrix]
+#     return result_matrix
+
+# if __name__ == "__main__":
+#     matrix = [
+#         [1, 2, 3],
+#         [4, 5, 6],
+#         [7, 8, 9]
+#     ]
+
+#     result = square_matrix_simple(matrix)
+#     print(result)

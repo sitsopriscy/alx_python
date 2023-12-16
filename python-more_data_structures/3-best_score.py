85fc83600619e0426e6005946f31df9c748295ed
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-16 02:14:31 -0800

    best score 03

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -2,9 +2,10 @@
 
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(a_dictionary) 
-    else:
-        return None
+        max_score = max(a_dictionary.values())
+        best_keys = [key for key, value in a_dictionary.items() if value == max_score]
+        return best_keys[0] if best_keys else None
+    return None
 #!/usr/bin/python3
 
 # def best_score(a_dictionary):

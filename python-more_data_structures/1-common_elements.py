90ff3c14257581d8d1061745d70970cf5444b77d
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-16 01:36:11 -0800

    common set

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,15 +1,18 @@
     #!/usr/bin/python3
-
 def common_elements(set_1, set_2):
-    # Use set intersection to find common elements
     common_set = set_1 & set_2
-    return common_set
+    return (common_set)
 
-if __name__ == "__main__":
-    # Test the function with your sets
-    set_1 = {"Python", "C", "Javascript"}
-    set_2 = {"Bash", "C", "Ruby", "Perl"}
+# def common_elements(set_1, set_2):
+#     # Use set intersection to find common elements
+#     common_set = set_1 & set_2
+#     return common_set
 
-    c_set = common_elements(set_1, set_2)
-    print(sorted(list(c_set)))
+# if __name__ == "__main__":
+#     # Test the function with your sets
+#     set_1 = {"Python", "C", "Javascript"}
+#     set_2 = {"Bash", "C", "Ruby", "Perl"}
+
+#     c_set = common_elements(set_1, set_2)
+#     print(sorted(list(c_set)))
 

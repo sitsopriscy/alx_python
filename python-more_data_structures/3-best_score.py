9e64a3c9e1d3c9a8852230071145c6b110205e05
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-16 01:59:35 -0800

    best score

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary:
-        max_score = max(a_dictionary.values())
-        best_keys = [key for key, value in a_dictionary.items() if value == max_score]
-        return best_keys[0] if best_keys else None
-    return None
+    if a_dictionary == 0:
+        return None
 
-if __name__ == "__main__":
-    a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-    best_key = best_score(a_dictionary)
-    print("Best score: {}".format(best_key))
+#!/usr/bin/python3
 
-    best_key = best_score(None)
-    print("Best score: {}".format(best_key))
+# def best_score(a_dictionary):
+#     if a_dictionary:
+#         max_score = max(a_dictionary.values())
+#         best_keys = [key for key, value in a_dictionary.items() if value == max_score]
+#         return best_keys[0] if best_keys else None
+#     return None
+
+# if __name__ == "__main__":
+#     a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+#     best_key = best_score(a_dictionary)
+#     print("Best score: {}".format(best_key))
+
+#     best_key = best_score(None)
+#     print("Best score: {}".format(best_key))

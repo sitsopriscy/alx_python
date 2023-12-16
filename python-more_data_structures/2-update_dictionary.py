4d2ddd022da47d2221674753aaaf62120d915e6b
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-16 01:51:24 -0800

    update dictionary

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,4 +1,13 @@
-# #!/usr/bin/python3
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """ Replaces or adds key/value in a dictionary. """
+    a_dictionary[key] = value
+    return a_dictionary
+
+
+#!/usr/bin/python3
+
 # from update_dictionary import update_dictionary
 
 # def print_sorted_dictionary(my_dict):
@@ -30,22 +39,22 @@ def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
 
-if __name__ == "__main__":
-    def print_sorted_dictionary(my_dict):
-        keys = sorted(my_dict.keys())
-        for k in keys:
-            print("{}: {}".format(k, my_dict[k]))
+# if __name__ == "__main__":
+#     def print_sorted_dictionary(my_dict):
+#         keys = sorted(my_dict.keys())
+#         for k in keys:
+#             print("{}: {}".format(k, my_dict[k]))
 
-    a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
-    new_dict = update_dictionary(a_dictionary, 'language', "Python")
-    print_sorted_dictionary(new_dict)
-    print("--")
-    print_sorted_dictionary(a_dictionary)
+#     a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
+#     new_dict = update_dictionary(a_dictionary, 'language', "Python")
+#     print_sorted_dictionary(new_dict)
+#     print("--")
+#     print_sorted_dictionary(a_dictionary)
 
-    print("--")
-    print("--")
+#     print("--")
+#     print("--")
 
-    new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-    print_sorted_dictionary(new_dict)
-    print("--")
-    print_sorted_dictionary(a_dictionary)
+#     new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+#     print_sorted_dictionary(new_dict)
+#     print("--")
+#     print_sorted_dictionary(a_dictionary)

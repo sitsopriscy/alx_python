f1bef9ce738b1ef945913ea6ecc2546eb8cc4648
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-20 12:47:47 -0800

    Base class 01

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,12 +1,22 @@
-# models/base.py
+""" This module contains the class Base """
 class Base:
-    # private class attribute
+    """private class attribute for class Base, _nb_objects"""
     __nb_objects = 0
-
+    
     # class constructor
     def __init__(self, id=None):
+        """
+        initializes an instance of class Base 
+        
+        Variables"
+        __nb_objects = 0
+        
+        Args:
+        id = None
+        
+        """
         # public instance attribute
-        if id is not None:
+        if id != None:
             self.id = id
         else:
             # increment __nb_objects and assign the new value to the public instance attribute id

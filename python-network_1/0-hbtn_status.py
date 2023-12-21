5ead6c2e7a65d83ad03971e57bb9d76c7dec4a3e
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2023-12-21 13:33:53 -0800

    What's my status 005

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,20 @@
 """ A script that fetches a URL  
     URL: https://alu-intranet.hbtn.io/status
+    Usage:
+    ./0-hbtn_status.py | cat -e
 """
 import requests
 
 def get_url_status(url):
-    
+    """
+    Fetches the status of the given URL using the requests package.
+
+    Args:
+        url (str): The URL to fetch.
+
+    Returns:
+        requests.models.Response: The response object.
+
+    Raises:
+        requests.exceptions.RequestException: If the request encounters an error.
+    """

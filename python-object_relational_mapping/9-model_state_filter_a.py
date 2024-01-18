8bd6df2229779dea02815e4d506a27789dccaaa1
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-18 14:56:58 -0800

    Contains `a`

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,3 +7,27 @@
 # Results must be sorted in ascending order by states.id
 # The results must be displayed as they are in the example below
 # Your code should not be executed when imported
+
+
+"""Lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    """importing from sys, model_state and sqlalchely"""
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    path = "mysql+mysqldb://{}:{}@localhost/{}"
+    engine = create_engine(path.format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for states in (
+        session.query(State).filter(State.name.like("%a%")).order_by(State.id).all()
+    ):
+        print("{}: {}".format(states.id, states.name))
+
+    session.close()

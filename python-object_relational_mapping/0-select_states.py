de171c961ea293082c445963acd451302d2aa2ca
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-18 13:27:54 -0800

    Filter states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,37 @@
-import MySQLdb
-from sys import argv
+# Write a script that lists all states from the database hbtn_0e_0_usa:
+# Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+# You must use the module MySQLdb (import MySQLdb)
+# Your script should connect to a MySQL server running on localhost at port 3306
+# Results must be sorted in ascending order by states.id
+# Results must be displayed as they are in the example below
+# Your code should not be executed when imported
 
 if __name__ == "__main__":
-    mysql_username = argv[1]
-    mysql_password = argv[2]
-    db_name = argv[3]
+    from sys import argv
+    import MySQLdb
 
-    connect = MySQLdb.connect(
-        host="localhost",
-        user=mysql_username,
-        passwd=mysql_password,
-        database=db_name,
-        port=3006,
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306
     )
 
-    cursor = connect.cursor()
-    query = "SELECT * FROM states ORDER BY id ASC"
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the SQL query to retrieve states
+    cursor.execute(
+        "SELECT * FROM states\
+                    WHERE name LIKE 'N%' COLLATE utf8mb4_bin\
+                    ORDER BY states.id ASC"
+    )
+
+    # Fetch all the rows
+    states = cursor.fetchall()
+
+    # Display the results
+    for state in states:
+        print(state)
+
+    # Close the cursor and connection
     cursor.close()
-    connect.close()
+    db.close()

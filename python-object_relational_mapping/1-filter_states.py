abab82325b28ce154386eb569b012c05c3d6ab1e
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-18 13:52:09 -0800

    Filter states002

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,10 +11,12 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    # Check if the correct number of command-line arguments is provided
     if len(argv) != 4:
         print("Usage: {} <username> <password> <database>".format(argv[0]))
         exit(1)
 
+    # Extract username, password, and database name from command-line arguments
     username, password, database = argv[1], argv[2], argv[3]
 
     # Connecting to the MySQL server
@@ -22,16 +24,20 @@ if __name__ == "__main__":
         host="localhost", port=3306, user=username, passwd=password, db=database
     )
 
+    # Create a cursor object to execute SQL queries
     cursor = db.cursor()
 
-    # Executing the SQL query to fetch states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'n%' ORDER BY id ASC"
+    # Executing the SQL query to fetch states starting with 'n' (case-insensitive)
+    query = "SELECT * FROM states WHERE LOWER(name) LIKE 'n%' ORDER BY id ASC"
     cursor.execute(query)
 
+    # Fetch all the rows returned by the query
     rows = cursor.fetchall()
 
+    # Print the fetched rows
     for row in rows:
         print(row)
 
+    # Close the cursor and the database connection
     cursor.close()
     db.close()

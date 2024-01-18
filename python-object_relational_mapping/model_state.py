ffb0055fedb12d902ad65fcb98ef9c08025c6235
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-18 14:31:01 -0800

    First state model9659

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,29 +13,17 @@
 #!/usr/bin/python3
 """Start link class to table in database 
 """
-import sys
-from model_state import Base, State
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+""" importing attributes from SQLAlchemy """
 
-if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
+Base = declarative_base()
 
-    Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
+class State(Base):
+    """creating class States that inherits from Base"""
 
-    session = Session()
-
-    states = session.query(State).all()
-
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-
-    session.close()
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)

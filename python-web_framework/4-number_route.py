fa1606695fa2c426cbc3601ac4564ef962c01963
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-24 13:03:40 -0800

    Python - Web framework - 1

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,10 @@
+# Copy the previous task to a new script that starts a Flask web application:
+# Your web application must be listening on 0.0.0.0, port 5000
+# Routes:
+# /: display “Hello HBNB!”
+# /hbnb: display “HBNB”
+# /c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+# /python/<text>: display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+# The default value of text is “is cool”
+# /number/<n>: display “n is a number” only if n is an integer
+# You must use the option strict_slashes=False in your route definition

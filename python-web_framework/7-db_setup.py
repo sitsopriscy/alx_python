425423be0622889f2f4055a310a26d83d3cb707f
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-24 13:32:16 -0800

    2 task 0

--- python-web_framework/7-db_setup.py
+++ python-web_framework/7-db_setup.py
@@ -1,1 +1,6 @@
 # Database Configuration and Flask App Setup
+# Part 1: Database Configuration
+# Install MySQL:
+# Ensure MySQL is installed on your system.
+# Create a Database:
+# Use MySQL commands to create a new database named alx_

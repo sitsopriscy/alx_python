af8e26c72e27070cdc80b7a130d9986bb7e78bce
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-25 13:37:54 -0800

    odd_or_even

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -14,13 +14,69 @@
 # You must use the option strict_slashes=False in your route definition
 
 
-"""to import flask"""
-from flask import Flask
+# """to import flask"""
+# from flask import Flask
+
+# """creates the variable application name"""
+# app = Flask(__name__)
+
+# """route to retrieve hello HBNB"""
+
+
+# @app.route("/", strict_slashes=False)
+# def hello_hbnb():
+#     return "Hello HBNB!"
+
+
+# @app.route("/hbnb", strict_slashes=False)
+# def hbnb():
+#     return "HBNB"
+
+
+# @app.route("/c/<text>", strict_slashes=False)
+# def c_text(text):
+#     return "C " + text.replace("_", " ")
+
+
+# @app.route("/python/<text>", strict_slashes=False)
+# @app.route("/python/", strict_slashes=False)
+# def python_text(text="is cool"):
+#     return "Python " + text.replace("_", " ")
+
+
+# @app.route("/number/<int:n>", strict_slashes=False)
+# def number(n):
+#     return "{} is a number".format(n)
+#     if not n:
+#         return 404
+
+
+# @app.route("/number_template/<int:n>", strict_slashes=False)
+# def number_template(n):
+#     return render_template("5-number.html", n=n)
+
+
+# @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+# def number_odd_even(n):
+#     if n % 2 == 0:
+#         return render_template("6-number_odd_or_even.html", n=n, x="even")
+#     else:
+#         return render_template("6-number_odd_or_even.html", n=n, x="odd")
+
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
+
+
+"""this module contains a script that creates web application using flask"""
+
+"""importing flask"""
+from flask import Flask, render_template
 
 """creates the variable application name"""
 app = Flask(__name__)
 
-"""route to retrieve hello HBNB"""
+"""retrieving route response"""
 
 
 @app.route("/", strict_slashes=False)

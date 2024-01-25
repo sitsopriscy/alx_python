54b72959ebd652fe9895f76d196c0c791f70230e
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-24 21:08:13 -0800

    Number template

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -10,3 +10,49 @@
 # /number_template/<n>: display a HTML page only if n is an integer:
 # H1 tag: “Number: n” inside the tag BODY
 # You must use the option strict_slashes=False in your route definition
+
+
+"""to import flask"""
+from flask import Flask
+
+"""creates the variable application name"""
+app = Flask(__name__)
+
+"""route to retrieve hello HBNB"""
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def python_text(text="is cool"):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+    if not n:
+        return 404
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

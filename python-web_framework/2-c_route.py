6fd367d32351a92a8651eb84fe5c54cda1ed18f1
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-24 21:02:38 -0800

    C is fun

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -6,3 +6,31 @@
 # /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
 # You must use the option strict_slashes=False in your route definition
 """
+
+
+"""importing flask"""
+from flask import Flask
+
+"""creates the variable application name"""
+app = Flask(__name__)
+
+"""route for retrieving hello HBNB"""
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

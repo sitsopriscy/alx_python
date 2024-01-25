d3552935654921b449c7b3d88c67bf41d8087669
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-24 20:59:26 -0800

    HBNB001

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -16,32 +16,6 @@
 
 
 # @app.route("/", strict_slashes=False)
-# def hbnb():
-#     return "Hello HBNB!"
-
-
-# """a route to retrieve HBNB!"""
-
-
-# @app.route("/hbnb", strict_slashes=False)
-# def hbnb():
-#     return "HBNB"
-
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
-
-
-# """importing flask"""
-# from flask import Flask
-
-# """creates the variable application name"""
-# app = Flask(__name__)
-
-# """route for retrieving hello HBNB"""
-
-
-# @app.route("/", strict_slashes=False)
 # def hello_hbnb():
 #     return "Hello HBNB!"
 
@@ -53,3 +27,29 @@
 
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=5000)
+
+
+"""importing flask"""
+from flask import Flask
+
+"""creates the variable application name"""
+app = Flask(__name__)
+
+"""route for retrieving hello HBNB"""
+
+
+@app.route("/", strict_slashes=False)
+def hbnb():
+    return "Hello HBNB!"
+
+
+"""a route to retrieve HBNB!"""
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

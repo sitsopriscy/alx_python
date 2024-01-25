f1f2afce70341ff843e9b229f15c2479b016129d
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-24 21:04:44 -0800

    python is cool 01

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -33,5 +33,11 @@ def c_text(text):
     return "C " + text.replace("_", " ")
 
 
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def python_text(text="is cool"):
+    return "Python " + text.replace("_", " ")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

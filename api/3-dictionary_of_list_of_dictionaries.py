a7049d6420ad9dbb4079f491cdcc25e18cfc77e9
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-02-25 10:19:15 -0800

    dict

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -6,9 +6,9 @@ Records all tasks from all employees
 Format must be: { "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ], "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ]}
 File name must be: todo_all_employees.json """
 
+import json
 import requests
 import sys
-import json
 
 
 def get_user_data(user_id):

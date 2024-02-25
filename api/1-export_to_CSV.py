04faf9890e1fec9d282e9a148ea2ee16a402d0a3
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-02-25 09:42:02 -0800

    Task11

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,54 +6,41 @@ Records all tasks that are owned by this employee
 Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
 File name must be: USER_ID.csv """
 
+#!/usr/bin/python3
+""" Script that uses JSONPlaceholder API to get information about employee """
 import csv
 import requests
 import sys
 
-
-def get_user_data(user_id):
+if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user_url = "{}users/{}".format(url, user_id)
-    response = requests.get(user_url)
-    return response.json()
 
+    # Get user ID from command line arguments
+    userid = sys.argv[1]
 
-def get_user_tasks(user_id):
-    url = "https://jsonplaceholder.typicode.com/"
-    todos_url = "{}todos?userId={}".format(url, user_id)
-    response = requests.get(todos_url)
-    return response.json()
+    # Fetch user data from API
+    user = "{}users/{}".format(url, userid)
+    res = requests.get(user)
+    json_o = res.json()
+    username = json_o.get("username")  # Store username
 
+    # Fetch todos for user from API
+    todos = "{}todos?userId={}".format(url, userid)
+    res = requests.get(todos)
+    tasks = res.json()
 
-def write_to_csv(user_id, username, tasks):
-    filename = "{}.csv".format(user_id)
+    # For each todo, store relevant data in l_task
+    l_task = []
+    for task in tasks:
+        l_task.append([userid, username, task.get("completed"), task.get("title")])
 
-    with open(filename, mode="w", newline="") as employee_file:
+    # Write data to CSV file
+    filename = "{}.csv".format(userid)
+    with open(filename, mode="w") as employee_file:
         employee_writer = csv.writer(
             employee_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL
         )
-
-        for task in tasks:
+        for task in l_task:
             employee_writer.writerow(
-                [user_id, username, task.get("completed"), task.get("title")]
-            )
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    user_id = int(sys.argv[1])
-
-    user_data = get_user_data(user_id)
-    username = user_data.get("username")  # Store username
-    user_tasks = get_user_tasks(user_id)
-
-    # For each task, store relevant data in l_task
-    l_task = []
-    for task in user_tasks:
-        l_task.append([user_id, username, task.get("completed"), task.get("title")])
-
-    # Write data to CSV file
-    write_to_csv(user_id, username, user_tasks)
+                task
+            )  # Write each list in l_task as a new row in the CSV file

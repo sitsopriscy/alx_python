1ef22b5ba05348ea6b55a3375acf293a75b88208
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-02-25 09:57:38 -0800

    Export 01

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -10,17 +10,20 @@ import csv
 import requests
 import sys
 
+
 def get_user_data(user_id):
     url = "https://jsonplaceholder.typicode.com/"
     user_url = "{}users/{}".format(url, user_id)
     response = requests.get(user_url)
     return response.json()
 
+
 def get_user_tasks(user_id):
     url = "https://jsonplaceholder.typicode.com/"
     todos_url = "{}todos?userId={}".format(url, user_id)
     response = requests.get(todos_url)
     return response.json()
+
 
 def display_user_progress(user_data, tasks):
     print("Employee {} is done with tasks".format(user_data.get("name")), end="")
@@ -30,21 +33,25 @@ def display_user_progress(user_data, tasks):
     for task in completed_tasks:
         print("\t {}".format(task.get("title")))
 
+
 def export_to_csv(user_id, user_data, tasks):
     filename = "{}.csv".format(user_id)
-    
-    with open(filename, 'w', newline='') as csvfile:
+
+    with open(filename, "w", newline="") as csvfile:
         fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        
-        writer.writeheader()
+        writer = csv.writer(csvfile)
+        writer.writerow(fieldnames)
+
         for task in tasks:
-            writer.writerow({
-                "USER_ID": user_data.get("id"),
-                "USERNAME": user_data.get("username"),
-                "TASK_COMPLETED_STATUS": str(task.get("completed")),
-                "TASK_TITLE": task.get("title")
-            })
+            writer.writerow(
+                [
+                    user_data.get("id"),
+                    user_data.get("username"),
+                    str(task.get("completed")),
+                    task.get("title"),
+                ]
+            )
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
@@ -54,16 +61,9 @@ if __name__ == "__main__":
     user_id = int(sys.argv[1])
 
     user_data = get_user_data(user_id)
-    if not user_data:
-        print(f"Error: User not found for ID {user_id}")
-        sys.exit(1)
-
     user_tasks = get_user_tasks(user_id)
 
     display_user_progress(user_data, user_tasks)
 
-    try:
-        export_to_csv(user_id, user_data, user_tasks)
-        print("Export to CSV: Success")
-    except Exception as e:
-        print(f"Export to CSV: Error - {e}")
+    export_to_csv(user_id, user_data, user_tasks)
+    print("CSV export completed.")
